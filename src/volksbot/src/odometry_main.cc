f85e946e4c9c68966c7f90cde665e8d0d8b207a8
fussel179 <riluzm@protonmail.com>
2021-08-05 14:37:43 +0200

    feat: Odometry node now stores current position in a temporary file

--- src/volksbot/src/odometry_main.cc
+++ src/volksbot/src/odometry_main.cc
@@ -1,8 +1,9 @@
 #include "volksbot/odometry.h"
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
   ros::init(argc, argv, "Odometry");
+
+  printf("Launching odometry node...\n");
 
   bool publish_tf = true;
 
