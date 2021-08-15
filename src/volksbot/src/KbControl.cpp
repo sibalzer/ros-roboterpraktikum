0f8e82402705049436012a695855beaa6737a421
fussel179 <riluzm@protonmail.com>
2021-08-15 03:53:29 +0200

    feat: Cleanup and partly rewrite keyboard controller

---
+++ src/volksbot/src/KbControl.cpp
@@ -0,0 +1,124 @@
+#include <fcntl.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <string.h>
+#include <termios.h>
+#include <unistd.h>
+
+#include <cmath>
+#include <stdexcept>
+
+#include "KbControl.h"
+#include "stdio.h"
+#include "sys/time.h"
+
+namespace volksbot
+{
+int kfd = 0;
+void quit(int signum)
+{
+  // return to the normal input mode
+  struct termios cooked;
+  tcgetattr(kfd, &cooked);
+  cooked.c_lflag |= (ICANON | ECHO);
+  cooked.c_cc[VEOL] = 0;
+  cooked.c_cc[VEOF] = 4;
+
+  tcsetattr(kfd, TCSANOW, &cooked);
+  ros::shutdown();
+}
+
+KbControl::KbControl(const char* loggingName) : loggingName_{ loggingName }
+{
+  velocity_.request.left = velocity_.request.right = 0.0;
+  speed_ = 0.0;
+  kfd = 0;
+
+  ROS_DEBUG_NAMED(loggingName, "Get parameters");
+  nh_.param<int>("looprate", rate_, 5);
+  nh_.param<std::string>("velocity_service", velSrcName_, "Controls");
+}
+
+void KbControl::run()
+{
+  char c;                     // current pressed character
+  char previous = KEYCODE_Q;  // last pressed character
+
+  // some terminal magic
+  struct termios cooked;
+  tcgetattr(kfd, &cooked);
+  cooked.c_lflag &= ~(ICANON | ECHO);
+  // Setting a new line, then end of file
+  cooked.c_cc[VEOL] = 1;
+  cooked.c_cc[VEOF] = 2;
+  tcsetattr(kfd, TCSANOW, &cooked);
+
+  // register interrupt handler
+  signal(SIGINT, quit);
+
+  ros::Rate loop_rate(5);
+  while (ros::ok())
+  {
+    c = KEYCODE_Q;
+    read(kfd, &c, 1);
+
+    handleKey(c, previous);
+    sendSpeed();
+
+    // ROS house keeping
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+}
+
+void KbControl::handleKey(const char c, char& previous)
+{
+  char final = c;
+
+  // handle faster and slower speeds
+  if (c == KEYCODE_A)
+  {
+    speed_ = std::min(speed_ + 10.0, 100.0);
+    final = previous;
+  }
+  else if (c == KEYCODE_Y)
+  {
+    speed_ = std::max(speed_ - 10.0, 0.0);
+    final = previous;
+  }
+
+  switch (final)
+  {
+    case KEYCODE_L:
+      velocity_.request.left = -speed_;
+      velocity_.request.right = speed_;
+      break;
+    case KEYCODE_R:
+      velocity_.request.left = speed_;
+      velocity_.request.right = -speed_;
+      break;
+    case KEYCODE_U:
+      velocity_.request.left = speed_;
+      velocity_.request.right = speed_;
+      break;
+    case KEYCODE_D:
+      velocity_.request.left = -speed_;
+      velocity_.request.right = -speed_;
+      break;
+    case KEYCODE_Q:
+      velocity_.request.left = 0;
+      velocity_.request.right = 0;
+      break;
+  }
+
+  previous = c;
+}
+
+void KbControl::sendSpeed()
+{
+  ROS_DEBUG_NAMED(loggingName_, "Motor vels: [%f, %f], speed: %f", velocity_.request.left, velocity_.request.right,
+                  speed_);
+  ros::service::call(velSrcName_, velocity_);
+}
+
+}  // namespace volksbot
---
+++ src/volksbot/src/KbControl.h
@@ -0,0 +1,89 @@
+#ifndef KB_CONTROL_H
+#define KB_CONTROL_H
+
+#include <ros/ros.h>
+
+// service
+#include "volksbot/velocities.h"
+
+#define KEYCODE_R 'r'  // 0x43
+#define KEYCODE_L 'l'  // 0x44
+#define KEYCODE_U 'u'  // 0x41
+#define KEYCODE_D 'd'  // 0x42
+#define KEYCODE_Q 'q'  // 0x71
+#define KEYCODE_A 'a'  // 0x61
+#define KEYCODE_Y 'y'  // 0x79
+
+namespace volksbot
+{
+/**
+ * The stdin keyboard file director.
+ */
+extern int kfd;
+
+/**
+ * A custom SIGINT handler to gracefully close the stdin file director
+ * and clean up the keyboard controller.
+ */
+void quit(int signum);
+
+class KbControl
+{
+public:
+  /**
+   * Creates a new keyboard controller
+   * that publishes motor velocities on keyboard inputs.
+   */
+  KbControl(const char* loggingName = "kb_control");
+
+  /**
+   * Runs the ros-node main loop, waits for keyboard inputs,
+   * parses them and send suitable motor velocities
+   */
+  void run();
+
+private:
+  const char* loggingName_;
+
+  /**
+   * The motor speed update rate in Hertz.
+   */
+  int rate_;
+
+  /**
+   * The name of the service to set the motor velocities.
+   */
+  std::string velSrcName_;
+
+  /**
+   * The default node handle for this object.
+   */
+  ros::NodeHandle nh_;
+
+  ros::ServiceClient client_;
+
+  /**
+   * The current velocities the motors should have.
+   * This variable is published when {@link sendSpeed} is called.
+   */
+  volksbot::velocities velocity_;
+
+  /**
+   * The current target speed adjusted via the keyboard.
+   */
+  double speed_;
+
+  /**
+   * Handles a parsed keyboard key input.
+   */
+  void handleKey(char c, char& previous);
+
+  /**
+   * Sends the current motor velocities to the motor controllers.
+   */
+  void sendSpeed();
+};
+
+}  // namespace volksbot
+
+#endif
